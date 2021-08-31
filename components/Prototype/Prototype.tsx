8839c67267cc11662626a572caff5552edea124a
Rob Easthope <hello@robeasthope.com>
2021-08-31 11:38:02 +0100

    Create Prototype.tsx

---
+++ components/Prototype/Prototype.tsx
@@ -0,0 +1,9 @@
+  import { themeClass, exampleStyle } from './styles.css.ts';
+
+export const Prototype = () => {
+  return (
+    <section className={themeClass}>
+      <h1 className={exampleStyle}>Hello world!</h1>
+    </section>
+  )
+}
