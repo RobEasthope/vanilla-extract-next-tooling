bd19f17ee35dbf9bd18e1228d477ca6016370a98
Rob Easthope <hello@robeasthope.com>
2021-08-31 11:53:31 +0100

    Revert "Add vanilla-extract-plugin-nextjs config"
    This reverts commit cfb3197b9b684803e940fad79d29aa76777dbe73.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withVanillaExtract = require("vanilla-extract-plugin-nextjs");
-
-module.exports = withVanillaExtract({
+module.exports = {
   reactStrictMode: true,
-});
+}
